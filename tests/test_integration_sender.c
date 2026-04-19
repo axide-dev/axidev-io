00486e0f113902d65199b020546866e0e2dcbe49
Zied Yousfi <zied.essaber@gmail.com>
2026-04-19 19:25:41 +0200

    test(integration): read sender input in raw mode on Linux
    - disable canonical mode while waiting for the emitted keys
    - collect the observed line character by character instead of relying on `fgets`
    - restore terminal settings before exiting

--- tests/test_integration_sender.c
+++ tests/test_integration_sender.c
@@ -1,30 +1,45 @@
-#include <axidev-io/c_api.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#ifdef _WIN32
-#include <Windows.h>
-static DWORD WINAPI send_thread_main(LPVOID user_data) {
-  (void)user_data;
-  Sleep(500);
-  axidev_io_keyboard_tap(
-      (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_Z, 0});
-  axidev_io_keyboard_tap(
-      (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_W, 0});
-  axidev_io_keyboard_tap(
-      (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_NUM1, 0});
-  axidev_io_keyboard_tap(
-      (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_ENTER, 0});
-  return 0;
-}
-#else
+#ifndef _WIN32
+#include <errno.h>
 #include <pthread.h>
+#include <termios.h>
 #include <unistd.h>
+
+static struct termios g_old_termios;
+static int g_termios_saved = 0;
+
+static int enable_raw_mode(void) {
+  struct termios t;
+
+  if (tcgetattr(STDIN_FILENO, &g_old_termios) != 0) {
+    perror("tcgetattr");
+    return 0;
+  }
+
+  g_termios_saved = 1;
+  t = g_old_termios;
+
+  t.c_lflag &= ~(ICANON | ECHO);
+  t.c_cc[VMIN] = 1;
+  t.c_cc[VTIME] = 0;
+
+  if (tcsetattr(STDIN_FILENO, TCSANOW, &t) != 0) {
+    perror("tcsetattr");
+    return 0;
+  }
+
+  return 1;
+}
+
+static void restore_terminal(void) {
+  if (g_termios_saved) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &g_old_termios);
+  }
+}
+
 static void *send_thread_main(void *user_data) {
   (void)user_data;
   usleep(500000);
+
   axidev_io_keyboard_tap(
       (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_Z, 0});
   axidev_io_keyboard_tap(
@@ -33,12 +48,14 @@ static void *send_thread_main(void *user_data) {
       (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_NUM1, 0});
   axidev_io_keyboard_tap(
       (axidev_io_keyboard_key_with_modifier_t){AXIDEV_IO_KEY_ENTER, 0});
+
   return NULL;
 }
 #endif
 
 int main(void) {
   char buffer[256];
+  size_t len = 0;
 
   if (!axidev_io_keyboard_initialize()) {
     fprintf(stderr, "sender init failed\n");
@@ -47,7 +64,9 @@ int main(void) {
 
   printf("Sender integration test\n");
   printf("Keep this terminal focused, then press ENTER.\n");
-  fgets(buffer, sizeof(buffer), stdin);
+  fflush(stdout);
+
+  fgets(buffer, sizeof(buffer), stdin); // validation manuelle du focus
 
 #ifdef _WIN32
   {
@@ -59,13 +78,39 @@ int main(void) {
 #else
   {
     pthread_t thread;
+    int ch;
+
+    if (!enable_raw_mode()) {
+      axidev_io_keyboard_free();
+      return EXIT_FAILURE;
+    }
+
     pthread_create(&thread, NULL, send_thread_main, NULL);
-    fgets(buffer, sizeof(buffer), stdin);
+
+    // on attend réellement les caractères reçus par le process
+    while (len + 1 < sizeof(buffer)) {
+      ch = getchar();
+      if (ch == EOF) {
+        break;
+      }
+
+      buffer[len++] = (char)ch;
+
+      if (ch == '\n' || ch == '\r') {
+        break;
+      }
+    }
+
+    buffer[len] = '\0';
+
     pthread_join(thread, NULL);
+    restore_terminal();
   }
 #endif
 
   printf("Observed line: %s\n", buffer);
+  fflush(stdout);
+
   axidev_io_keyboard_free();
   return EXIT_SUCCESS;
 }
